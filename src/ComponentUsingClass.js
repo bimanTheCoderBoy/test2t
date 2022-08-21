9763e1e26bf251a5a05ea0d9dc41db1375c80fdd
Biman Kumar Das <daskumarbiman2020@gmail.com>
2022-08-21 22:12:51 +0000

    Update ComponentUsingClass.js

--- src/ComponentUsingClass.js
+++ src/ComponentUsingClass.js
@@ -2,7 +2,7 @@
 import React, { Component } from "react";
 class Hehe extends Component {
   render() {
-    return <h1>How are You</h1>;
+    return <h1>How are Y</h1>;
   }
 }
 export default Hehe;
